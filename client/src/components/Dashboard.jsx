68f8ae4448f0f2c694690032d4debf8c45f7156e
Patrick Patel <patrickpatel96@gmail.com>
2022-05-23 18:10:36 -0600

    update graphs moreee

--- client/src/components/Dashboard.jsx
+++ client/src/components/Dashboard.jsx
@@ -38,19 +38,55 @@ const Dashboard = ({ price, totalBTC, distributions }) => (
               chartType="LineChart"
               loader={<div>Loading Chart</div>}
               data={[
-                ['Month', 'Total Distributions (BTC)', 'Total Distributions (USD)'],
-                [1, distributions[0], distributions[0]*price],
-                [2, distributions[1], distributions[1]*price],
-                [3, distributions[2], distributions[2]*price],
-                [4, distributions[3], distributions[3]*price],
-                [5, distributions[4], distributions[4]*price],
-                [6, distributions[5], distributions[5]*price],
-                [7, distributions[6], distributions[6]*price],
-                [8, distributions[7], distributions[7]*price],
-                [9, distributions[8], distributions[8]*price],
-                [10, distributions[9], distributions[9]*price],
-                [11, distributions[10], distributions[10]*price],
-                [12, distributions[11], distributions[11]*price],
+                ['Month', 'Total Distributions (BTC)'],
+                [1, distributions[0]],
+                [2, distributions[1]],
+                [3, distributions[2]],
+                [4, distributions[3]],
+                [5, distributions[4]],
+                [6, distributions[5]],
+                [7, distributions[6]],
+                [8, distributions[7]],
+                [9, distributions[8]],
+                [10, distributions[9]],
+                [11, distributions[10],],
+                [12, distributions[11],],
+              ]}
+              options={{
+                hAxis: {
+                  title: 'Month',
+                },
+                vAxis: {
+                  title: 'BTC',
+                },
+                fontName: 'Verdana',
+                fontSize: 16,
+              }}
+              rootProps={{ 'data-testid': '1' }}
+            />
+          </Card>
+        </Col>
+        <Col className="col-6">
+          <Card style={{background: "rgba(255,153,0,0.2)"}}>
+            <Chart
+              style={{background: "rgba(255,153,0,0.2)"}}
+              height={'500px'}
+              chartType="LineChart"
+              loader={<div>Loading Chart</div>}
+              data={[
+                ['Month', 'Total Distributions (USD)'],
+                [1, distributions[0]*price],
+                [2, distributions[1]*price],
+                [3, distributions[2]*price],
+                [4, distributions[3]*price],
+                [5, distributions[4]*price],
+                [6, distributions[5]*price],
+                [7, distributions[6]*price],
+                [8, distributions[7]*price],
+                [9, distributions[8]*price],
+                [10, distributions[9]*price],
+                [11, ddistributions[10]*price],
+                [12, ddistributions[11]*price],
               ]}
               options={{
                 hAxis: {

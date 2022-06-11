f21af9be47ff52db849fcd74bb8cecc636876824
Patrick Patel <patrickpatel96@gmail.com>
2022-06-11 11:33:16 -0600

    add login error message

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -10,7 +10,7 @@ class Login extends React.Component {
     this.state = {
       "email": "",
       "password": "",
-      message: ""
+      message: "",
       redirectHome: false,
       redirectForgotPassword: false,
     };

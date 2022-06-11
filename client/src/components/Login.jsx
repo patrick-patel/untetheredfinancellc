eb8a8b0ae4979f02fe6176ab62ce2a9c542ffca2
Patrick Patel <patrickpatel96@gmail.com>
2022-06-11 17:49:17 -0600

    work on login routing

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -90,7 +90,7 @@ class Login extends React.Component {
               <Form>
                 <Form.Group className="mb-3" controlId="formBasicEmail">
                   <Form.Label>Email</Form.Label>
-                  <Form.Control type="email" placeholder="Enter Email" name="email" value={this.state.email} onChange={this.onChange.bind(this)} style={{backgroundColor: "rgba(255,153,0,0.2)"}} required />
+                  <Form.Control type="email" placeholder="Enter Email" name="email" value={this.state.email} onChange={this.props.onChange()} style={{backgroundColor: "rgba(255,153,0,0.2)"}} required />
                   <Form.Text className="text-muted">
                     We'll never share your email with anyone else.
                   </Form.Text>
@@ -98,16 +98,16 @@ class Login extends React.Component {
 
                 <Form.Group className="mb-3" controlId="formBasicPassword">
                   <Form.Label>Password</Form.Label>
-                  <Form.Control type="password"  placeholder="Enter Password" name="password" value={this.state.password} onChange={this.onChange.bind(this)} style={{backgroundColor: "rgba(255,153,0,0.2)"}} required />
+                  <Form.Control type="password"  placeholder="Enter Password" name="password" value={this.state.password} onChange={this.props.onChange()} style={{backgroundColor: "rgba(255,153,0,0.2)"}} required />
                 </Form.Group>
                 <Row>
                   <Col>
-                    <Button className="mb-2" variant="dark" type="submit" style={{cursor: "pointer", background: "#050038", marginLeft: 50, width: "75%"}} onClick={this.login.bind(this)}>
+                    <Button className="mb-2" variant="dark" type="submit" style={{cursor: "pointer", background: "#050038", marginLeft: 50, width: "75%"}} onClick={this.props.login()}>
                       Login
                     </Button>
                   </Col>
                   <Col>
-                    <Button variant="dark" type="submit" style={{cursor: "pointer", background: "#050038", marginLeft: 50, width: "75%"}} onClick={this.forgotPassword.bind(this)}>
+                    <Button variant="dark" type="submit" style={{cursor: "pointer", background: "#050038", marginLeft: 50, width: "75%"}} onClick={this.props.forgotPassword()}>
                       Forgot Password
                     </Button>
                   </Col>

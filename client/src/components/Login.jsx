d2997b77f1890640da692c9765c2f90e6b6bc84a
Patrick Patel <patrickpatel96@gmail.com>
2022-06-12 18:39:57 -0600

    resetting login jsx to show card

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -35,42 +35,7 @@ class Login extends React.Component {
       return (
         <div style={{background: "rgba(255,153,0,0.2)"}}>
           <br></br>
-          <Container fluid style={{padding: 90}}>
-          <br></br>
-            <Row>
-              <Col>
-                <h3 className="display-3" style={{color: "#050038", textDecoration: "none", margin: "auto", width: "75%"}}><b>Member Login</b></h3>
-                <p className="lead" style={{color: "#050038", textDecoration: "none", margin: "auto", width: "75%", fontSize: "24px"}}><i>View your member dashboard, company metrics</i></p>
-                <p className="lead" style={{color: "#050038", textDecoration: "none", margin: "auto", width: "75%", fontSize: "24px"}}><i>and update your account information</i></p>
-              </Col>
-              <Col>
-                {this.props.message.length > 0 ? <Alert key={this.props.messageType} variant={this.props.messageType}>{this.props.message}</Alert> : null}
-                <Card style={{background: "rgba(255,153,0,0.2)"}}>
-                  <Container style={{padding: 20}}>
-                    <Form>
-                      <Form.Group className="mb-3" controlId="formBasicEmail">
-                        <Form.Label>Email</Form.Label>
-                        <Form.Control type="email" placeholder="Enter Email" name="email" value={this.props.email} onChange={this.onChange.bind(this)} style={{backgroundColor: "rgba(255,153,0,0.2)"}} required />
-                        <Form.Text className="text-muted">We'll never share your email with anyone else</Form.Text>
-                      </Form.Group>
-                      <Form.Group className="mb-3" controlId="formBasicPassword">
-                        <Form.Label>Password</Form.Label>
-                        <Form.Control type="password"  placeholder="Enter Password" name="password" value={this.props.password} onChange={this.onChange.bind(this)} style={{backgroundColor: "rgba(255,153,0,0.2)"}} required />
-                      </Form.Group>
-                      <Row>
-                        <Col>
-                          <Button className="mb-2" variant="dark" type="submit" style={{cursor: "pointer", background: "#050038", marginLeft: 50, width: "75%"}} onClick={this.login.bind(this)}>Login</Button>
-                        </Col>
-                        <Col>
-                          <Button variant="dark" type="submit" style={{cursor: "pointer", background: "#050038", marginLeft: 50, width: "75%"}} onClick={this.forgotPassword.bind(this)}>Forgot Password</Button>
-                        </Col>
-                      </Row>
-                    </Form>
-                  </Container>
-                </Card>
-              </Col>
-            </Row>
-          </Container>
+
         </div>
       )
     }
